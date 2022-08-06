81aca8dc63faaef5dc397b220d7aa1c32da2cc15
Manti <67705577+mantikafasi@users.noreply.github.com>
2022-08-06 22:31:08 +0300

    change the way oauth authrorization works

--- Utils/Utils.ts
+++ Utils/Utils.ts
@@ -2,35 +2,40 @@ import { settings, toast } from "ittai";
 import { findByProps } from "ittai/webpack";
 import GenericToast from "ittai/toast/ToastWrapper";
 import { getClientMod } from "ittai/utilities";
-const openOAuth2Modal = findByProps("openOAuth2Modal"); 
+import React from "react";
+const { getOAuth2AuthorizeProps } = findByProps("openOAuth2Modal"); 
+const { OAuth2AuthorizeModal } = findByProps('OAuth2AuthorizeModal')
+const { openModal } = findByProps('openModal', 'useModalsStore')
 
 
 export function authorize(callback?:any) {
     
-    var props = openOAuth2Modal.getOAuth2AuthorizeProps("https://discord.com/api/oauth2/authorize?client_id=915703782174752809&redirect_uri=https%3A%2F%2Fmanti.vendicated.dev%2FURauth&response_type=code&scope=identify")
-    openOAuth2Modal.openOAuth2Modal(props,() => {
-        if (tempOpen !== undefined) window.open = tempOpen;
-    })
-
-    // for some reason patching doesnt work so I temporarily override the function
-    var tempOpen = window.open;
-    window.open = function (url?:string):void {
-        if (url?.startsWith("https://manti.vendicated.dev")) {
-            
-            fetch(url + "&returnType=json&clientMod=" + getClientMod()).then(res => {res.json().then(res => {
-                if (res.status === 0) {
-                    settings.set("token", res.token);
-                    showToast("Successfully logged in!")
-                    callback?.()
-                } else if (res.status === 1) {
-                    showToast("An Error Occured while logging in.")
-                }
-            })})
-            window.open = tempOpen;
-        } else {
-            tempOpen(url);
+    var opts = getOAuth2AuthorizeProps("https://discord.com/api/oauth2/authorize?client_id=915703782174752809&redirect_uri=https%3A%2F%2Fmanti.vendicated.dev%2FURauth&response_type=code&scope=identify")
+    
+    openModal((props:any) =>
+        React.createElement(OAuth2AuthorizeModal, {
+        ...props,
+        ...opts,
+        responseType: 'code',
+        cancelCompletesFlow: false,
+        callback: (c:any) => {
+            try {
+                const url = new URL(c)
+                fetch(url + "&returnType=json&clientMod=" + getClientMod()).then(res => {res.json().then(res => {
+                    if (res.status === 0) {
+                        settings.set("token", res.token);
+                        showToast("Successfully logged in!")
+                        callback?.()
+                    } else if (res.status === 1) {
+                        showToast("An Error Occured while logging in.")
+                    }
+                })})
+            } catch (e) {
+                console.log(e)
+            }
         }
-    }
+        })
+    )
 }
 
 export function showToast(text:string) {
